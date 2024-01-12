ead26311dd6c2d7ea19765fd004cdca3c4f18538
iwan <iwanwang2312@gmail.com>
2024-01-12 19:53:44 +0800

    Update inbox-contract.ts

--- src/messages/inbox-contract.ts
+++ src/messages/inbox-contract.ts
@@ -205,8 +205,8 @@ export class InboxContract {
     console.log(envelope);
     this.logger.debug(`l1BlockNumber: ${ethers.BigNumber.from(rollupStateProofData.l1BlockNumber)}`);
     this.logger.debug(`outputIndex: ${ethers.BigNumber.from(rollupStateProofData.outputIndex)}`);
-    this.logger.log(`outputProof:${outputProof}`);
-    console.log(outputProof);
+    this.logger.log(`outputProof:`);
+    console.log(JSON.stringify(outputProof));
     this.logger.debug(`mptInclusionProof:`);
     console.log(mptInclusionProof);
     
